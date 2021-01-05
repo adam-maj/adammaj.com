c3a35e64b329ef993aeefcd337e7202011c69bff
adam-maj <mr.adam.maj@gmail.com>
2021-01-05 14:03:05 -0500

    Analytics w/ dangerously set inner HTML

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,21 @@ import '../styles/globals.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  function injectGA() {
-    if (typeof window == 'undefined') {
-      return;
-    }
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      window.dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-  
-    gtag('config', 'G-M61FCSRJR9');
-  };
-
   return (
     <>
       <Head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-M61FCSRJR9"></script>
-        <script>{injectGA()}</script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M61FCSRJR9');
+            `,
+          }}
+        />
 
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />

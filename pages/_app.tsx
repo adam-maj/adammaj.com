cc8da3d187eaca486809d8044ede652d86a2b2ed
adam-maj <mr.adam.maj@gmail.com>
2023-02-18 01:27:31 -0500

    Update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,15 @@ import { DefaultSeo } from "next-seo";
 import posthog from "posthog-js";
 import React from "react";
 import { useRouter } from "next/router";
+import { Lora } from "@next/font/google";
+
+const lora = Lora({ subsets: ["latin"], display: "swap" });
 
 const theme = extendTheme(
   {
     fonts: {
-      heading: `"Lora", serif`,
-      body: `"Lora", sans-serif`,
+      heading: lora.style.fontFamily,
+      body: lora.style.fontFamily,
     },
   },
   withProse({
@@ -55,24 +58,26 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <ChakraProvider theme={theme}>
-      <DefaultSeo
-        title="Adam Majmudar"
-        description="I'm a constant learner and aspiring technical generalist. I'm also a founding enginer at thirdweb and on gap year from the University of Pennsylvania."
-        openGraph={{
-          title: "Adam Majmudar",
-          description:
-            "I'm a constant learner and aspiring technical generalist. I'm also a founding enginer at thirdweb and on gap year from the University of Pennsylvania.",
-          images: [
-            {
-              url: "https://adammaj.com/og-image-dark.jpg",
-              type: "image/jpeg",
-            },
-          ],
-          siteName: "Adam Majmudar",
-        }}
-      />
-      {getLayout(<Component {...pageProps} />)}
-    </ChakraProvider>
+    <main>
+      <ChakraProvider theme={theme}>
+        <DefaultSeo
+          title="Adam Majmudar"
+          description="I'm a constant learner and aspiring technical generalist. I'm also a founding enginer at thirdweb and on gap year from the University of Pennsylvania."
+          openGraph={{
+            title: "Adam Majmudar",
+            description:
+              "I'm a constant learner and aspiring technical generalist. I'm also a founding enginer at thirdweb and on gap year from the University of Pennsylvania.",
+            images: [
+              {
+                url: "https://adammaj.com/og-image-dark.jpg",
+                type: "image/jpeg",
+              },
+            ],
+            siteName: "Adam Majmudar",
+          }}
+        />
+        {getLayout(<Component {...pageProps} />)}
+      </ChakraProvider>
+    </main>
   );
 }

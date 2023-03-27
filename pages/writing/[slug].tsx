4170663676ac468b2dd84c36a4227e54e72695dc
Adam Majmudar <adam@Adams-MacBook-Pro.local>
2023-03-26 18:33:45 -0700

    Update post style and titles

--- pages/writing/[slug].tsx
+++ pages/writing/[slug].tsx
@@ -28,7 +28,7 @@ const Post: NextPageWithLayout<PostProps> = ({ post }) => {
           ],
         }}
       />
-      <Flex direction="column" gap={1}>
+      <Flex direction="column" gap={2}>
         <Heading size="lg">{post.metadata.title}</Heading>
         <Prose>
           <MDXRemote compiledSource={post.source} />

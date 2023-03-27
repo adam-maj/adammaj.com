62fec1908f9d5907b60ddde4d58b045f69a779c2
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2023-03-26 18:09:33 -0700

    Update writing page structure (#4)

--- pages/writing/index.tsx
+++ pages/writing/index.tsx
@@ -1,8 +1,15 @@
-import { Heading, Stack, Flex, Text, Divider } from "@chakra-ui/react";
+import {
+  Heading,
+  Link,
+  Flex,
+  Text,
+  Stack,
+  HStack,
+  Divider,
+} from "@chakra-ui/react";
 import { getAllPostData, Post } from "../../lib/writing";
 import type { NextPageWithLayout } from "next";
 import Layout from "../../components/Layout";
-import Link from "next/link";
 import { NextSeo } from "next-seo";
 
 interface WritingProps {
@@ -13,29 +20,24 @@ const Writing: NextPageWithLayout<WritingProps> = ({ posts }) => {
   return (
     <>
       <NextSeo title="Writing | Adam Majmudar" />
-      <Flex direction="column" align="flex-start">
+      <Flex direction="column" align="flex-start" width="100%" gap={3}>
+        <Divider width="100%" />
         {posts.map((post) => (
-          <Stack width="100%" key={post.title}>
-            <Link href={post.url} target={post.external ? "_blank" : "_self"}>
-              <Stack mb={4} width="100%">
-                <Divider margin="8px 0 !important" width="100%" />
-                <Stack>
-                  <Heading
-                    as="h2"
-                    size="md"
-                    marginTop="8px !important"
-                    mb="0px !important"
-                  >
-                    {post.title}
-                  </Heading>
-                  <Text my={0}>{post.description}</Text>
-                  <Text color="gray.500" mt={0}>
-                    {post.date}
-                  </Text>
-                </Stack>
-              </Stack>
-            </Link>
-          </Stack>
+          <>
+            <Stack width="100%" align="flex-start" spacing={1}>
+              <Link
+                href={post.url}
+                target={post.external ? "_blank" : "_self"}
+                color="blue.600"
+              >
+                <Text>{post.title}</Text>
+              </Link>
+              <Text fontSize="sm" textAlign={"right"} color="gray.500">
+                {post.date}
+              </Text>
+            </Stack>
+            <Divider width="100%" />
+          </>
         ))}
       </Flex>
     </>
